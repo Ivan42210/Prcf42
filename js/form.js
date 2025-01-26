ccf6c5af546595d828ac6f6fd9674bdff2f4edb5
Ivan42210 <ivan.cha.42@outlook.fr>
2025-01-26 12:59:34 +0100

    creation of the form

--- js/form.js
+++ js/form.js
@@ -1,4 +1,15 @@
+const contactBtn = document.querySelector('.btn_form');
+const contactForm = document.querySelector('.form_section');
+const closeBtn = document.querySelector('.close_btn');
 
-function validatForm(){
-    
-}
+console.log(contactBtn);
+
+contactBtn.addEventListener('click', () => {
+    contactForm.classList.remove('hidden');
+    contactForm.classList.add('opened');
+});
+
+closeBtn.addEventListener('click', () => {
+    contactForm.classList.add('hidden');
+    contactForm.classList.remove('opened');
+});
