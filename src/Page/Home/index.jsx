b02f79ef9e28cd08642fbf60e1ec88a5630d0857
Ivan42210 <ivan.cha.42210@outlook.fr>
2025-01-27 15:57:56 +0100

    creation

--- src/Page/Home/index.jsx
+++ src/Page/Home/index.jsx
@@ -1,10 +1,16 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Banner from "../../Components/Banner";
 
 export default function Home (){
 
+    const btnList = [{
+
+    }]
+
     return(
         <>
             <Banner />
+            <FontAwesomeIcon icon={faXTwitter} /> 
         </>
     )
 }
