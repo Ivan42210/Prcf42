04d5e3b0ed9e323b51f1e17ddc466797e2f7a8e9
Ivan42210 <ivan.cha.42210@outlook.fr>
2025-01-27 14:28:31 +0100

    index css

--- src/Components/BtnBlocks/index.jsx
+++ src/Components/BtnBlocks/index.jsx
@@ -1,8 +1,16 @@
-export default function BtnBlock (){
+export default function BtnBlock ({children}){
+
+    const style = {
+        display: 'flex',
+        margin: '1em',
+        justifyContent: 'space-between'
+    }
 
     return(
         <>
-            
+            <section style={style}>
+                {children}
+            </section>
         </>
     )
 }
