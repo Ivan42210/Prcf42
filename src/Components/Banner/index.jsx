4686744877d12fc9320fe56719e0751e727d5198
Ivan42210 <ivan.cha.42210@outlook.fr>
2025-01-27 13:10:59 +0100

    banner creation

--- src/Components/Banner/index.jsx
+++ src/Components/Banner/index.jsx
@@ -3,10 +3,17 @@ export default function Banner ({image, alt, logo, altLogo}){
 
     const style = {
         section: {
-            justifyContent: 'flex',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems:'center',
             width: '100%',
             height: '40vh',
             background: 'black'
+        },
+        divLogo:{
+            width: '50px',
+            height: '50px',
+            background: 'blue',
         }
     }
 
@@ -14,7 +21,7 @@ export default function Banner ({image, alt, logo, altLogo}){
         <>
             <section style={style.section}>
                 <img src={image} alt={alt} />
-                <div>
+                <div style={style.divLogo}>
                     <img src={logo} alt={altLogo} />
                 </div>
             </section>
