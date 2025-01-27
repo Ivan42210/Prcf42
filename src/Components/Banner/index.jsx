be03eee13b2988d25995c423ae576644d20817af
Ivan42210 <ivan.cha.42210@outlook.fr>
2025-01-27 13:01:25 +0100

    creation style

--- src/Components/Banner/index.jsx
+++ src/Components/Banner/index.jsx
@@ -1,9 +1,18 @@
 
 export default function Banner ({image, alt, logo, altLogo}){
 
+    const style = {
+        section: {
+            justifyContent: 'flex',
+            width: '100%',
+            height: '40vh',
+            background: 'black'
+        }
+    }
+
     return(
         <>
-            <section>
+            <section style={style.section}>
                 <img src={image} alt={alt} />
                 <div>
                     <img src={logo} alt={altLogo} />
--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -3,7 +3,7 @@ export default function Form (){
 
     return(
         <>
-        
+            
         </>
     )
 }
