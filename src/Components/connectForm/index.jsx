1bb7aad53f434feac93c10f5324e1beb0a7dc675
Ivan42210 <ivan.cha.42@outlook.fr>
2025-03-13 20:01:42 +0100

    modifications

---
+++ src/Components/connectForm/index.jsx
@@ -0,0 +1,14 @@
+export default function connectForm() {
+
+
+    return (
+        <div>
+            <h1>Connectez-vous</h1>
+            <form>
+                <input type="text" placeholder="email"/>
+                <input type="password" placeholder="Mot de passe"/>
+                <button type="submit">Se connecter</button>
+            </form>
+        </div>
+    )
+}
