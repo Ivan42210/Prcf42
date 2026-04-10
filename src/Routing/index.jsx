f9dee7f42d6cc967a53f72072ec1e7a5c186450e
Ivan42210 <ivan.cha.42@outlook.fr>
2026-04-10 21:17:09 +0000

    login page

--- src/Routing/index.jsx
+++ src/Routing/index.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom'
 import './Routing.css'
 import Home from '../Page/Home'
+import LoginPage from '../Page/LoginPage'
+import DashboardPage from '../Page/DashboardPage'
 
 
 export default function Routing (){
@@ -11,7 +13,8 @@ export default function Routing (){
         < Router>
             <Routes>
                 <Route exact path={'/'} element={<Home/>}/>
-                
+                 <Route path="/login" element={<LoginPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} /> 
             </Routes>
         </Router>
     </>
